468ff63dbb90210624e9b3d4c660350cbf1f2873
Belbut <80793911+Belbut@users.noreply.github.com>
2025-07-27 13:28:17 +0100

    fix: use datetime to store takeoff_time

--- db/migrate/20250727114449_create_flights.rb
+++ db/migrate/20250727114449_create_flights.rb
@@ -1,8 +1,8 @@
 class CreateFlights < ActiveRecord::Migration[8.0]
   def change
     create_table :flights do |t|
-      t.time :takeoff_time
-      t.integer :duration
+      t.datetime :takeoff_time
+      t.integer :duration_in_minutes
       t.references :departure_airport, null: false, foreign_key: true
       t.references :arrival_airport, null: false, foreign_key: true
 
