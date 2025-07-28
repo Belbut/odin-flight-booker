421a0f216f5ca394e6402d77eb02af0e94313daa
Belbut <80793911+Belbut@users.noreply.github.com>
2025-07-28 22:54:53 +0100

    feat: add flight search form view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,8 @@
 class FlightsController < ApplicationController
   def index
+    @airports_options = Airport.all.map { |airport| [airport.name, airport.id] }
+    puts "********************************* #{@airports_options}"
   end
 
-  def show
-  end
+  def show; end
 end
