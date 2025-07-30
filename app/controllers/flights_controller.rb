be7608a91bd1eea1f2a3260737b43a68d4aca554
Belbut <80793911+Belbut@users.noreply.github.com>
2025-07-30 13:51:19 +0100

    feat: restrict date content on dropdown

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airports_options = Airport.all.map { |airport| [airport.name, airport.id] }
-    puts "********************************* #{@airports_options}"
+    @flights_options = Flight.all.map { |flight| [flight.formatted_takeoff_date, flight.id] }
   end
 
   def show; end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def formatted_takeoff_date
+    takeoff_time.strftime("%d/%m/%Y")
+  end
 end
