645f2b57ecc158a7cd80030b826f8ecf7dda859f
Belbut <80793911+Belbut@users.noreply.github.com>
2025-07-31 17:23:11 +0100

    feat: add estimated time en-route to flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,13 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport'
 
   def formatted_takeoff_date
-    takeoff_time.strftime("%d/%m/%Y")
+    takeoff_time.strftime('%d/%m/%Y')
+  end
+
+  # ete = estimated time in route
+  def ete
+    hours = duration_in_minutes / 60
+    minutes = duration_in_minutes % 60
+    "#{hours.to_s.rjust(2, '0')}H#{minutes.to_s.rjust(2, '0')}"
   end
 end
