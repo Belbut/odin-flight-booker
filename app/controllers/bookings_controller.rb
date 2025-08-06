b4bd637fc30bda4378e14c82e6643c7573e9e3f6
Belbut <80793911+Belbut@users.noreply.github.com>
2025-08-06 17:34:16 +0100

    feat: add #create controller for nested form logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,30 @@
 class BookingsController < ApplicationController
   def new
-    @booking = Booking.new
-
     selected_flights_ids = params[:flight].keys
     @flights = Flight.find(selected_flights_ids)
+
+    @booking = Booking.new(flights: @flights)
 
     number_of_passengers = params[:number_passengers].to_i
     number_of_passengers.times { @booking.passengers.build }
   end
 
-  def create; end
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.flights = Flight.find(params[:flight_ids])
+
+    if @booking.save
+      redirect_to root_path, notice: 'Booking created!'
+    else
+      render :new, notice: 'Failed to save the booking'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      passengers_attributes: %i[name email]
+    )
+  end
 end
