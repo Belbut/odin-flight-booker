831137da8939434b8f66f29edb22336d2c9764e0
Belbut <80793911+Belbut@users.noreply.github.com>
2025-09-11 17:18:43 +0100

    refactor: complete refactor to permit js to save nested form into db

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -19,9 +19,12 @@ export default class extends Controller {
 
       const passenger_name_input = passenger_node.getElementById("passengers_new_name_")
       passenger_name_input.id = `passengers_new_name_${index}`
+      passenger_name_input.name = `booking[passengers_attributes][${index}][name]`
 
       const passenger_email_input = passenger_node.getElementById("passengers_new_email_")
       passenger_email_input.id = `passengers_new_email_${index}`
+      passenger_email_input.name = `booking[passengers_attributes][${index}][email]`
+
       
       section.appendChild(passenger_node)
     }
