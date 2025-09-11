9933866ef068b1ab67c7252e0c32fa7ddb303efa
Belbut <80793911+Belbut@users.noreply.github.com>
2025-09-11 17:37:18 +0100

    feat: use controller to add new passengers

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,21 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="add-passenger"
+// Connects to data-controller="passenger"
 export default class extends Controller {
-  static targets = ["btn"]
+  static targets = [""]
 
   connect() {
     console.log("passenger controller connected")
-    const template = document.querySelector('template')
-    const section = document.getElementById("passenger_section")
-
-    let numberOfPassenger = document.getElementById("number_passengers").value
+    const numberOfPassenger = document.getElementById("number_passengers").value
 
     for (let index = 0; index < numberOfPassenger; index++) {  
-      const passenger_node = document.importNode(template.content, true)
+      this.appendNewPassenger(index+1) // print index from 1
+    }
+  }
+
+  add() {
+    let numberOfPassenger = document.getElementById("number_passengers")
+    numberOfPassenger.value++;
+
+    this.appendNewPassenger(numberOfPassenger.value)
+  }
+
+  appendNewPassenger(index) {
+    const template = document.querySelector('template')
+    const section = document.getElementById("passenger_section")
+    const passenger_node = document.importNode(template.content, true)
 
       const passenger_number = passenger_node.querySelector(".index")
-      passenger_number.innerHTML = index + 1
+      passenger_number.innerHTML = index
 
       const passenger_name_input = passenger_node.getElementById("passengers_new_name_")
       passenger_name_input.id = `passengers_new_name_${index}`
@@ -27,6 +38,5 @@ export default class extends Controller {
 
       
       section.appendChild(passenger_node)
-    }
   }
 }
